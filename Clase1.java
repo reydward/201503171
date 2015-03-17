2f2a5cb59f8a32a4a6baea64c6ec25a5534b014b
Eduard Reyes <reydward@gmail.com>
2015-03-17 18:09:43 -0500

    Cambios Clase1

--- Clase1.java
+++ Clase1.java
@@ -3,6 +3,7 @@ public class Clase1 {
     public static void main(String[] args) {
         System.out.println("First time");
         System.out.println("Second developer");
+        System.out.println("First developer");
     }
 
 }
