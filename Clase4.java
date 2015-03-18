95b2d46cd60fce756947b7314b451efd445bf2cc
Eduard Reyes <reydward@gmail.com>
2015-03-18 12:44:14 -0500

    Nueva línea en feature_1

--- Clase4.java
+++ Clase4.java
@@ -3,6 +3,8 @@ public class Clase4 {
     public static void main(String[] args) {
         System.out.println("Feature_1 branch");
         System.out.println("Change from github");
+
+        System.out.println("Feature_1 branch");
     }
 
 }
